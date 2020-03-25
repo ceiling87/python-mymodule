875690a40b240441ca40a08eb8f5d8f74dd63280
ceiling87 <62639040+ceiling87@users.noreply.github.com>
2020-03-25 15:23:14 +0800

    Update mymodule.py

--- mymodule.py
+++ mymodule.py
@@ -10,6 +10,17 @@ def get_weather(city):
   weather = json.loads(result)
   return weather ["main"]["temp'] - 273.15
 
+def postal_lookup(postal_code):
+    sock = urlopen("http://api.postcodes.io/postcodes/" + postal_code)
+    result = sock.read()
+    sock.close()
+    details = json.loads(result)
+    return(details["result"]["latitude"], detials["results"]["longitude"])
+
 if__name__== "__main__":
   degrees = get_weather("OSLO")
   print("Weather in Oslo is %.2f degrees Celsius" % degrees)
+  location = postal_lookip("B323PP")
+  print(location)                      
+
+                          
